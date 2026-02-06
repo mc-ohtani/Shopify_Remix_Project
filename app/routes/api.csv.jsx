ee40ad01234dd83849dbdfec03570dd55c5ce827
DESKTOP-U2BSKLM\ohtani <ms.ohtani@gmail.com>
2026-02-06 15:17:52 +0900

    コメントアウトしていた部分を破棄

--- app/routes/api.csv.jsx
+++ app/routes/api.csv.jsx
@@ -1,20 +1,4 @@
-// import { convertCsv } from "../../services/csv";
 
-// export async function action({ request }) {
-//   const formData = await request.formData();
-//   const file = formData.get("file");
-//   const target = formData.get("target");
-
-//   const csvText = await file.text();
-
-//   const result = await convertCsv(csvText, target);
-
-//   return new Response(result, {
-//     headers: {
-//       "Content-Type": "text/csv",
-//     },
-//   });
-// }
 import { parse } from "csv-parse/sync";
 import { convertCsv } from "../services/csv/index";
 
--- app/services/csv/index.js
+++ app/services/csv/index.js
@@ -1,16 +1,4 @@
-// import { convertFreee } from "./freee";
-// import { convertMoneyforward } from "./moneyforward";
 
-// export function convertCsv(csvText, target) {
-//   switch (target) {
-//     case "freee":
-//       return convertFreee(csvText);
-//     case "moneyforward":
-//       return convertMoneyforward(csvText);
-//     default:
-//       throw new Error("Unsupported target");
-//   }
-// }
 import { convertFreee } from "./freee.js";
 import { convertMoneyForward } from "./moneyforward.js";
 
