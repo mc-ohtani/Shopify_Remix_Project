9efa51cecc0113b6f60df9a3e677ceb6473a1d54
DESKTOP-U2BSKLM\ohtani <ms.ohtani@gmail.com>
2026-02-20 17:34:21 +0900

    db設定更新

--- app/db.server.js
+++ app/db.server.js
@@ -1,11 +1,17 @@
 import { PrismaClient } from "@prisma/client";
 
-if (process.env.NODE_ENV !== "production") {
-  if (!global.prismaGlobal) {
-    global.prismaGlobal = new PrismaClient();
-  }
-}
+const prisma =
+  global.prismaGlobal ||
+  new PrismaClient({
+    datasources: {
+      db: {
+        url: process.env.DATABASE_URL,
+      },
+    },
+  });
 
-const prisma = global.prismaGlobal ?? new PrismaClient();
+if (process.env.NODE_ENV !== "production") {
+  global.prismaGlobal = prisma;
+}
 
 export default prisma;
