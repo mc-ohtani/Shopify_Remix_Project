89d1b4072e21a470ba7978b110d10093882614c3
DESKTOP-U2BSKLM\ohtani <ms.ohtani@gmail.com>
2026-02-24 16:46:42 +0900

    no message

--- app/db.server.js
+++ app/db.server.js
@@ -1,15 +1,11 @@
 import { PrismaClient } from "@prisma/client";
 
-// 念のため、独自変数と標準変数の両方をチェック
-const connectionString = process.env.SUPABASE_DATABASE_URL || process.env.DATABASE_URL;
-
 const prisma =
   global.prismaGlobal ||
   new PrismaClient({
-    // 💡 実行時に強制的に接続先とエンジンを指定する
     datasources: {
       db: {
-        url: connectionString,
+        url: process.env.DATABASE_URL,
       },
     },
   });
