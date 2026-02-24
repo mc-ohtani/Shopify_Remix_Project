f5f9a3bc1101181d7396d9d8a39b4e5691ea1b0f
DESKTOP-U2BSKLM\ohtani <ms.ohtani@gmail.com>
2026-02-24 16:02:19 +0900

    条件を追加して緩和対応

--- app/db.server.js
+++ app/db.server.js
@@ -1,11 +1,15 @@
 import { PrismaClient } from "@prisma/client";
 
+// 念のため、独自変数と標準変数の両方をチェック
+const connectionString = process.env.SUPABASE_DATABASE_URL || process.env.DATABASE_URL;
+
 const prisma =
   global.prismaGlobal ||
   new PrismaClient({
+    // 💡 実行時に強制的に接続先とエンジンを指定する
     datasources: {
       db: {
-        url: process.env.SUPABASE_DATABASE_URL,
+        url: connectionString,
       },
     },
   });
