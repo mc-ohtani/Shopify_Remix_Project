a53da3e9ac9dca938962626a93ff50d3329e09ea
DESKTOP-U2BSKLM\ohtani <ms.ohtani@gmail.com>
2026-02-25 17:38:04 +0900

    URL直接

--- app/db.server.js
+++ app/db.server.js
@@ -1,17 +1,15 @@
+// app/db.server.ts
 import { PrismaClient } from "@prisma/client";
 
-const prisma =
-  global.prismaGlobal ||
-  new PrismaClient({
-    datasources: {
-      db: {
-        url: process.env.DATABASE_URL,
-      },
-    },
-  });
+// テスト用：URLを直接書く（昨日書き換えたURLと同じもの）
+const databaseUrl = "postgres://postgres:xZObYWL6F2sR8B1J@db.pvoilibkvkbsbcwwilqi.supabase.co:6543/postgres?pgbouncer=true&connection_limit=1";
 
-if (process.env.NODE_ENV !== "production") {
-  global.prismaGlobal = prisma;
-}
+const prisma = new PrismaClient({
+  datasources: {
+    db: {
+      url: databaseUrl,
+    },
+  },
+});
 
 export default prisma;
